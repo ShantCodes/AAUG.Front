c60a8c86c4cd2244124f1697e37c06cf33b6923a
Shant <Shant>
2024-11-09 18:57:35 +0330

    minor changes

--- src/_components/newsComponents/NewsCard.jsx
+++ src/_components/newsComponents/NewsCard.jsx
@@ -26,7 +26,7 @@ const NewsCard = () => {
     };
 
     return (
-        <div className="p-4 max-w-sm mx-auto rounded-lg">
+        <div className="p-4 max-w-sm mx-auto rounded-lg sticky">
             <h1 className="text-2xl font-bold text-left text-gray-900 mb-6 flex items-center">
                 News
                 <BookOpenIcon className="w-6 h-6 ml-2 text-red-600" />
--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -1,3 +1,4 @@
 const BASE_API_URL = 'http://localhost:37523/api';
+// const BASE_API_URL = 'http://localhost:5138/api';
 
 export default BASE_API_URL;
